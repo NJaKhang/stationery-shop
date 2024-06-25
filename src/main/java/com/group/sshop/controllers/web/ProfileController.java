2a997b5f2b79ccb4c8fa19405e34bfe72368e7d7
phaml <vuphamletruong@gmail.com>
2024-06-25 09:06:22 +0700

    - add profile page - add profile link at header

--- src/main/java/com/group/sshop/controllers/web/ProfileController.java
+++ src/main/java/com/group/sshop/controllers/web/ProfileController.java
@@ -1,11 +1,19 @@
 package com.group.sshop.controllers.web;
 
+import com.group.sshop.models.entities.User;
+import com.group.sshop.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class UserController {
+@RequiredArgsConstructor
+public class ProfileController {
+    private final UserDetailsService userDetailsService;
 
     @GetMapping("/profile")
     public ModelAndView profile() {
